7cd8b209497b55d28442abf59602e08f133e84c6
Sam Robinson <110217039+SamRobinson-2511@users.noreply.github.com>
2022-11-07 01:21:50 -0600

    commit tree 3191216ea25d1d37354e9b3e104f2f041f3f1933

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solutions in this file
+// const gifts = ['teddy bear', 'drone', 'doll']
+
+// function wrapGifts(gift){
+//     console.log(`Wrapped ${gift} and added a bow!`)
+// }
+
+// wrapGifts(gifts[0])
+
+// for (let age = 30; age < 40; age++){
+//     console.log(`I'm ${age} years old. Happy birthday to me!`)
+// }
+
+// const gifts = ['teddy bear', 'drone', 'doll']
+
+// function wrapGifts(gifts){
+//     for (let i = 0; i < gifts.length; i++){
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`){
+//     return gifts;
+//         }
+//     }
+// }
+
+// wrapGifts(gifts)
+
+
+
+
+function writeCards(input, event){
+    let messages = new Array();
+    for (let i = 0; i<input.length;i++){
+        messages.push(`Thank you, ${input[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages;
+}
+
+function countDown(i){
+    while(i>=0){
+        console.log(i--)
+    }
+}
